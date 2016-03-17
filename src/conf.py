61ef27ac269b4b9e12a6288d21574ec3b859fab5
François Schmidts <francois.schmidts@gmail.com>
2016-03-17 16:04:08 +0100

    continuing

--- src/conf.py
+++ src/conf.py
@@ -1,67 +1,93 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-""" Program variables.
-
-This file contain the variables used by the application.
-"""
-import os
 import logging
+from collections import defaultdict
 
-API_ROOT = '/api/v2.0'
-LANGUAGES = {'en': 'English', 'fr': 'French'}
-TIME_ZONE = {'en': 'US/Eastern', 'fr': 'Europe/Paris'}
+def to_log_level(level):
+    return {'debug': logging.DEBUG,
+            'info': logging.INFO,
+            'warn': logging.WARN,
+            'error': logging.ERROR,
+            'fatal': logging.FATAL}.get(str(level).lower(), logging.WARN)
 
-ON_HEROKU = int(os.environ.get('HEROKU', 0)) == 1
 
-config = None  # for now
+OPTIONS = (
+        {'key': 'api_root', 'default': '/api/v2.0', 'edit': False},
+        {'key': 'languages', 'edit': False,
+            'default': {'en': 'English', 'fr': 'French'}},
+        {'key': 'time_zone', 'edit': False,
+            'default': {'en': 'US/Eastern', 'fr': 'Europe/Paris'}},
 
-SELF_REGISTRATION = config.getboolean('misc', 'self_registration')
-PLATFORM_URL = config.get('misc', 'platform_url')
-RECAPTCHA_PUBLIC_KEY = config.get('misc', 'recaptcha_public_key')
-RECAPTCHA_PRIVATE_KEY = config.get('misc',
-                                    'recaptcha_private_key')
-LOG_PATH = os.path.abspath(config.get('misc', 'log_path'))
-NB_WORKER = config.getint('misc', 'nb_worker')
-API_LOGIN = config.get('crawler', 'api_login')
-API_PASSWD = config.get('crawler', 'api_passwd')
+        {'key': 'platform_url', 'default': 'http://0.0.0.0:5000',
+            'help': 'At what address will JARR be available'},
+        {'key': 'sqlalchemy_database_uri'},
+        {'key': 'default_max_error', 'type': int, 'default': 6, 'edit': False},
+        {'key': 'error_threshold', 'type': int, 'default': 3, 'edit': False},
+        {'key': 'webserver.host'},
+        {'key': 'webserver.port', 'type': int},
+        {'key': 'webserver.secret'},
 
-SQLALCHEMY_DATABASE_URI = config.get('database', 'database_url')
+        {'key': 'log.level', 'type': to_log_level,
+            'default': 'warn', 'test': 'debug'},
+        {'key': 'log.type', 'default': ''},
+        {'key': 'log.path', 'default': ''},
 
-USER_AGENT = config.get('crawler', 'user_agent')
-RESOLVE_ARTICLE_URL = config.getboolean('crawler',
-                                        'resolve_article_url')
-DEFAULT_MAX_ERROR = config.getint('crawler',
-                                  'default_max_error')
-ERROR_THRESHOLD = int(DEFAULT_MAX_ERROR / 2)
+        {'key': 'crawler.login', 'default': 'admin'},
+        {'key': 'crawler.passwd', 'default': 'admin'},
+        {'key': 'crawler.nb_worker', 'type': int, 'default': 2, 'test': 1},
+        {'key': 'crawler.type', 'default': 'http', 'edit': False},
+        {'key': 'crawler.resolve_article_url', 'type': bool, 'default': False},
+        {'key': 'crawler.user_agent',
+                'default': 'https://github.com/jaesivsm/JARR'},
 
-CRAWLING_METHOD = config.get('crawler', 'crawling_method')
+        {'key': 'readability_key'},
+        {'key': 'auth.allow_signup', 'default': True, 'type': bool},
+        {'key': 'auth.recaptcha_public_key'},
+        {'key': 'auth.recaptcha_private_key'},
+        {'key': 'oauth.allow_signup', 'default': True, 'type': bool},
+        {'key': 'oauth.twitter.id'},
+        {'key': 'oauth.twitter.secret'},
+        {'key': 'oauth.facebook.id'},
+        {'key': 'oauth.facebook.secret'},
+        {'key': 'oauth.google.id'},
+        {'key': 'oauth.google.secret'},
 
-LOG_LEVEL = {'debug': logging.DEBUG,
-             'info': logging.INFO,
-             'warn': logging.WARN,
-             'error': logging.ERROR,
-             'fatal': logging.FATAL}[config.get('misc', 'log_level')]
+        {'key': 'notification.email'},
+        {'key': 'notification.host'},
+        {'key': 'notification.starttls'},
+        {'key': 'notification.port'},
+        {'key': 'notification.login'},
+        {'key': 'notification.password'},
+)
 
-WEBSERVER_HOST = config.get('webserver', 'host')
-WEBSERVER_PORT = config.getint('webserver', 'port')
-WEBSERVER_SECRET = config.get('webserver', 'secret')
 
-CDN_ADDRESS = config.get('cdn', 'cdn_address')
-READABILITY_KEY = config.get('misc', 'readability_key')
-OAUTH = {'twitter': {'id': config.get('misc', 'twitter_id'),
-                     'secret': config.get('misc', 'twitter_secret')},
-         'facebook': {'id': config.get('misc', 'facebook_id'),
-                      'secret': config.get('misc', 'facebook_secret')},
-         'google': {'id': config.get('misc', 'google_id'),
-                    'secret': config.get('misc', 'google_secret')},
-         }
+class Config:
 
-NOTIFICATION_EMAIL = config.get('notification', 'notification_email')
-NOTIFICATION_HOST = config.get('notification', 'host')
-NOTIFICATION_STARTTLS = config.getboolean('notification', 'starttls')
-NOTIFICATION_PORT = config.getint('notification', 'port')
-NOTIFICATION_USERNAME = config.get('notification', 'username')
-NOTIFICATION_PASSWORD = config.get('notification', 'password')
-POSTMARK_API_KEY = config.get('notification', 'postmark_api_key')
+    def __init__(self, options, test=False):
+        self.__options = options
+        self.__test = test
 
-WEBZINE_ROOT = config.get('webserver', 'webzine_root')
+    def __browse_keys(self):
+        for option in self.__options:
+            yield option['key'].replace('.', '_').upper()
+
+    def _dump(self):
+        def defaultdict_factory():
+            return defaultdict(defaultdict_factory)
+        to_dump = defaultdict(defaultdict_factory)
+        for option in self.__options:
+            if getattr(self, option['key'], None):
+                value = getattr(self, option['key'])
+            elif self.__test and 'test' in option:
+                value = option['test']
+            elif 'default' in option:
+                value = option['default']
+            else:
+                value = None
+            if '.' in option['key']:
+                tmp_d = to_dump
+                for sub_key in option['key'].split('.'):
+                    if not option['key'].endswith(sub_key):  # skiping last one
+                        tmp_d = to_dump[sub_key]
+                tmp_d[option['key'].split('.')[-1]] = value
+            else:
+                to_dump[option['key']] = value
+        return to_dump
